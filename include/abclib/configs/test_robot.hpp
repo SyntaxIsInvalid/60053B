87a98e8b889be1a9daadf388d17bcaef24d99720
SyntaxIsInvalid <syntaxisinvalid910@gmail.com>
2025-11-06 23:18:51 -0500

    updated units for test robot config

--- include/abclib/configs/test_robot.hpp
+++ include/abclib/configs/test_robot.hpp
@@ -13,10 +13,10 @@ namespace abclib::robot_config {
     // Sensor ports
     constexpr int8_t IMU_PORT = 9;
     
-    // Physical dimensions
-    constexpr double WHEEL_DIAMETER_INCHES = 3.25;
-    constexpr double TRACK_WIDTH_INCHES = 14.0;
-    constexpr double Y_TRACKER_OFFSET_INCHES = 7.0;
+    // Physical dimensions (using typed units)
+    inline constexpr units::Distance WHEEL_DIAMETER = units::Distance::from_inches(3.25);
+    inline constexpr units::Distance TRACK_WIDTH = units::Distance::from_inches(14.0);
+    inline constexpr units::Distance Y_TRACKER_OFFSET = units::Distance::from_inches(7.0);
     
     // Motor configurations
     inline hardware::motor_group_config get_left_motor_config() {
@@ -27,7 +27,9 @@ namespace abclib::robot_config {
             .kPv = 0.18,
             .kIv = 0.25,
             .kDv = 0.00,
-            .enable_voltage_compensation = true
+            .enable_voltage_compensation = true,
+            .compensation_nominal = units::Voltage::from_volts(12.0),
+            .compensation_min_battery = units::Voltage::from_volts(11.5)
         };
     }
     
@@ -39,7 +41,9 @@ namespace abclib::robot_config {
             .kPv = 0.18,
             .kIv = 0.25,
             .kDv = 0.00,
-            .enable_voltage_compensation = true
+            .enable_voltage_compensation = true,
+            .compensation_nominal = units::Voltage::from_volts(12.0),
+            .compensation_min_battery = units::Voltage::from_volts(11.5)
         };
     }
     
@@ -52,8 +56,10 @@ namespace abclib::robot_config {
         return control::PIDConstants(5, 0, 0);
     }
     
-    // Chassis config values
+    // Turn-in-place feedforward constants (for turn_to_heading_profiled)
     constexpr double TURN_IN_PLACE_KS = 1.278592;
     constexpr double TURN_IN_PLACE_KV = 0.170242;
     constexpr double TURN_IN_PLACE_KA = 0.012877;
+    
+    // Ramsete controller constants (for path following)
 }
